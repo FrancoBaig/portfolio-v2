501d96156b18495dc9290a65e2bf86e427351186
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-07-27 16:37:06 -0300

    Added: custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,13 @@ module.exports = {
             "grey-dark": "#212121",
             grey: "#7A7A7A",
         },
+        screens: {
+            sd: "500px",
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+        },
     },
     plugins: [],
 };
