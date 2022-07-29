f8df5a499f71178b0671d0af565ad658f242c1ea
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-07-29 12:44:29 -0300

    mobile navigation bar animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,88 @@
-import React from "react";
-import { Disclosure } from "@headlessui/react";
+import React, { useState, useRef } from "react";
+
+// icons
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+// helper
+import { useDimensions } from "../helper/use-dimensios";
+
+// motion
+import { motion } from "framer-motion";
 
 const Section = ({ text }) => {
     return (
-        <p className="text-grey font-bold px-1 rounded hover:text-white my-2 cursor-pointer">
+        <p className="text-grey font-bold text-xl px-1 rounded hover:text-white cursor-pointer">
             {text}
         </p>
     );
 };
 
+const sidebar = {
+    open: (height = 1000) => ({
+        clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
+        transition: {
+            type: "spring",
+            stiffness: 20,
+            restDelta: 2,
+        },
+    }),
+    closed: {
+        clipPath: "circle(30px at 40px 40px)",
+        transition: {
+            type: "spring",
+            stiffness: 400,
+            damping: 40,
+        },
+    },
+};
+
 function Navbar({ sections }) {
+    const [isOpen, setIsOpen] = useState(false);
+    const containerRef = useRef(null);
+    const { height } = useDimensions(containerRef);
+
     return (
-        <Disclosure as="nav" className="sm:flex sm:items-center sm:justify-end">
-            {({ open }) => (
-                <>
-                    <Disclosure.Button className="inline-flex items-center justify-center p-2 mb-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:hidden">
-                        {open ? (
-                            <XIcon className="w-8 h-8 text-white" />
-                        ) : (
-                            <MenuIcon className="w-8 h-8 text-white" />
-                        )}
-                    </Disclosure.Button>
-                    <div className="hidden sm:flex gap-2">
+        <motion.nav
+            className="sm:flex sm:items-center sm:justify-end"
+            animate={isOpen ? "open" : "closed"}
+            custom={height}
+            ref={containerRef}
+        >
+            <motion.div
+                variants={sidebar}
+                className={
+                    "absolute top-1 left-1 bottom-0 bg-black w-full z-20 sm:hidden"
+                }
+            />
+            <div
+                className="inline-flex items-center justify-center p-2 mb-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white sm:hidden"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                {isOpen ? (
+                    <XIcon className="w-8 h-8 text-white z-30" />
+                ) : (
+                    <MenuIcon className="w-8 h-8 text-white z-30" />
+                )}
+            </div>
+
+            {isOpen ? (
+                <div className="relative z-30 sm:hidden">
+                    <div className="flex flex-col gap-3 ">
                         {sections.map((opt, index) => (
                             <Section key={index} text={opt} />
                         ))}
                     </div>
-                    <Disclosure.Panel className="sm:hidden">
-                        <div>
-                            {sections.map((opt, index) => (
-                                <Section key={index} text={opt} />
-                            ))}
-                        </div>
-                    </Disclosure.Panel>
-                </>
+                </div>
+            ) : (
+                ""
             )}
-        </Disclosure>
+
+            <div className="hidden sm:flex gap-2">
+                {sections.map((opt, index) => (
+                    <Section key={index} text={opt} />
+                ))}
+            </div>
+        </motion.nav>
     );
 }
 
