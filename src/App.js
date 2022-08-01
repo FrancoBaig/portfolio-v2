3c9525d533a991e75bc486acda6432c812bbc6a2
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-08-01 17:22:55 -0300

    Added: skills section

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React from "react";
 import "./App.css";
 
-// Image
-import bwprofile from "./assets/img/bwprofile.png";
-
 // Components
 import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 import About from "./components/About";
 import Hero from "./components/Hero";
 
-const data = {
-    img: {
-        normal: bwprofile,
-    },
-    name: "Franco Baigorria",
-    title: "Full-Stack Developer",
-    description: `Lorem Ipsum has been the industry's standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries, but also the leap into electronic
-
-                `,
-    urls: {
-        linkedin: "...",
-        github: "...",
-        email: "...",
-    },
-    sections: ["About", "Projects", "Skills", "Education", "Contact"],
-};
+// Data (remove)
+import data from "./data.json";
 
 function App() {
     return (
@@ -42,6 +23,7 @@ function App() {
                 </div>
                 <About data={data} />
                 <Projects data={data} />
+                <Skills skills={data.skills} />
             </div>
         </>
     );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,23 +1,72 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+// Motion
+import { useAnimation, motion, useInView } from "framer-motion";
+
+// Parallax
+import { Parallax } from "react-scroll-parallax";
+
+const container = {
+    hidden: {},
+    show: {
+        rotate: 0,
+        transition: {
+            staggerChildren: 0.2,
+            delayChildren: 0.3,
+        },
+    },
+};
+
+const item = {
+    hidden: { scale: 0, top: 100 },
+    show: { scale: 1, top: 30 },
+};
 
 const Skill = ({ skill }) => {
     return (
-        <div className="flex flex-col items-center mx-4">
-            <p className="font-bold sm:text-lg">{skill.title}</p>
-            <img src={skill.img} className="w-20 sm:w-28" alt={skill.title} />
-        </div>
+        <Parallax speed={15}>
+            <div>
+                <motion.div
+                    variants={item}
+                    className="flex flex-col items-center mx-4"
+                >
+                    <p className="font-bold sm:text-lg">{skill.title}</p>
+                    <img
+                        src={skill.img}
+                        className="w-20 sm:w-28"
+                        alt={skill.title}
+                    />
+                </motion.div>
+            </div>
+        </Parallax>
     );
 };
 
 function Skills({ skills }) {
+    const controls = useAnimation();
+    const ref = useRef(null);
+    const isInView = useInView(ref);
+
+    useEffect(() => {
+        if (isInView) {
+            controls.start("show");
+        }
+    }, [controls, isInView]);
+
     return (
-        <div className="h-fit md:h-screen bg-white text-black p-5 flex flex-col ">
-            <h2 className="title self-center">Skills</h2>
-            <div className="max-w-screen-md mx-auto grid gap-y-8  gap-x-20 grid-cols-2 sm:gap-y-12 sm:grid-cols-4">
+        <div className="h-fit md:h-screen bg-dark-grey text-white p-5 flex flex-col ">
+            <h2 className="title self-center mb-28">Skills</h2>
+            <motion.div
+                ref={ref}
+                initial="hidden"
+                animate={controls}
+                variants={container}
+                className="max-w-screen-md mx-auto grid gap-y-4 gap-x-20 grid-cols-2 sm:gap-y-12 sm:grid-cols-4"
+            >
                 {skills.map((skill) => (
                     <Skill skill={skill} key={skill.id} />
                 ))}
-            </div>
+            </motion.div>
         </div>
     );
 }
