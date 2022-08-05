67022c8c8ef4e8c636cbc715c038e212d0cdf3d7
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-08-05 14:21:29 -0300

    Fixed: emoji position in mobile view

--- src/helper/emojiPositions.js
+++ src/helper/emojiPositions.js
@@ -4,10 +4,10 @@ const positions = [
     "top-1/4 left-3/4 md:left-1/2 md:ml-28 md:-mt-15",
     "-mt-16 top-1/4 left-3/4 ml-6 md:left-3/4 md:mt-40",
     "bottom-1/4 left-1/4 -ml-20 -mb-8 md:ml-20 md:-mb-20",
-    "bottom-1/2 left-1/4 -ml-20 -mb-8 md:-mb-20 md:-ml-40",
+    "bottom-1/2 left-1/4 -ml-20 -mb-28 md:-mb-20 md:-ml-40",
     "top-1/4 left-2/4 -ml-4 -mt-16 -mb-8 md:top-3/4 md:-mt-5",
     "top-1/4 left-1/4 -mt-16 -ml-20 -mb-8 md:top-1/4 md:-mt-15",
-    "top-3/4 right-1/3 md:top-1/3 md:right-3/4",
+    "top-3/4 right-2/3 md:top-1/3 md:right-3/4",
     "top-3/4 right-1/3 md:top-1/3 md:right-1/3 md:-ml-20",
 ];
 
